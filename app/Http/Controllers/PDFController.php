2469e8210710b0e2439721901cf5ef6d1d279a0c
oggiesutrisna <40566790+oggiesutrisna@users.noreply.github.com>
2021-07-22 20:20:47 +0800

    adding dompdf/phpword

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pasien;
 use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\Metadata\Settings;
 use PhpOffice\PhpWord\TemplateProcessor;
+use PhpOffice\PhpWord\Writer\PDF\DomPDF;
+use PhpOffice\PhpWord\Writer\PDF;
 
 class PDFController extends Controller
 {
-    public function createPDF($id) {
-        // Pasien::find($id);
-        $template = new TemplateProcessor(public_path('result.docx'));
+    public function createPDF(Pasien $id) {
+        $domPdfPath = base_path('vendor/dompdf/dompdf');
+        Settings::setPdfRendererPath($domPdfPath);
+        Settings::setPdfRendererName('DomPDF');
 
-        $SaveDocPath = public_path('new-result.docx');
+        $pasien = Pasien::find($id);
+
+        $template = new TemplateProcessor(public_path('result.docx'));
         $template->setValue('nosurat', $pasien->nosurat);
         $template->setValue('name', $pasien->name);
         $template->setValue('dob', $pasien->dob);
@@ -23,17 +29,16 @@ class PDFController extends Controller
         $template->setValue('jenis_pemeriksaan', $pasien->jenis_pemeriksaan);
         $template->setValue('nationality', $pasien->nationality);
 
+        $SaveDocPath = public_path('new-result.docx');
         $content = IOFactory::load($SaveDocPath);
+        // saving the result
         $savePdfPath = public_path('new_result.pdf');
         // exists
         if ( file_exists($savePdfPath)) {
             unlink($savePdfPath);
         }
-
-        $PdfWriter = IOFactory::createWriter($content, '.pdf');
-
+        $PdfWriter = IOFactory::createWriter($content, 'PDF');
         header("Content-Disposition: Attachment; filename=$pasien->name .'pdf'");
         $PdfWriter->save("php://output");
-        echo 'kekw';
     }
 }
