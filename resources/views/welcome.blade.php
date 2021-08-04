f06ec39f5e0173c0ac1cd58685175019290e451d
Extensia <sutrisna.oggie@gmail.com>
2021-08-04 11:49:40 +0800

    redesign welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,80 +12,27 @@
         <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{asset('assets-dk/css/styles.css')}}" rel="stylesheet" />
     </head>
-    <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top">DokterOne</a>
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{route('login')}}">Login</a></li>
-                    </ul>
-                </div>
+        <body class="d-flex h-100 text-center text-dark bg-white">
+            <img src="https://source.unsplash.com/1000x1000/?tech,nature" />
+
+            <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+              <header class="mb-auto">
+              </header>
+
+              <main class="px-3">
+                <h1>Welcome</h1>
+                <p class="lead">Faskes Unicare</p>
+                <p class="lead">
+                  <a href="{{route('login')}}" class="btn btn-lg btn-secondary fw-bold border-white bg-black">Login</a>
+                </p>
+              </main>
+
+              <footer class="mt-auto text-dark-50">
+                  <p>Built with 💖 by <a href="https://twitter.com/@oggiesutrisna"> Oggie Sutrisna </a></p>
+              </footer>
             </div>
-        </nav>
-        <!-- Masthead-->
-        <header class="masthead bg-primary text-white text-center">
-            <div class="container d-flex align-items-center flex-column">
-                <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="{{asset('assets-dk/assets/img/avataaars.svg')}}" alt="..." />
-                <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Selamat Datang</h1>
-                <!-- Icon Divider-->
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Welcome to DokterOne, an extraordinary application that you ever imagine</p>
-                <br />
-            </div>
-        </header>
-        <footer class="footer text-center">
-            <div class="container">
-                <div class="row">
-                    <!-- Footer Location-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Developed By :</h4>
-                        <p class="lead mb-0">
-                            <a href="https://twitter.com/@oggiesutrisna">Oggie Sutrisna</a>
-                            <br />
-                        </p>
-                    </div>
-                    <!-- Footer Social Icons-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Interested?</h4>
-                        <p class="lead mb-0">
-                            Contact Us Letsgo!
-                            <br />
-                        </p>
-                    </div>
-                    <!-- Footer About Text-->
-                    <div class="col-lg-4">
-                        <h4 class="text-uppercase mb-4">Interested?</h4>
-                        <p class="lead mb-0">
-                            Contact Us Letsgo!
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="{{asset('assets-dk/css/styles.css')}}"></script>
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     </body>
 </html>
